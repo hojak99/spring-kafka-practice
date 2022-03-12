79575215b225c70666eae2b633999f8513aa6033
jangho.kwon <jangho.kwon@yanolja.com>
2022-03-12 20:44:45 +0900

    init project

---
+++ src/main/java/com/hojak99/kafkapractice/Hojak99KafkaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.hojak99.kafkapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hojak99KafkaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hojak99KafkaPracticeApplication.class, args);
+	}
+
+}
